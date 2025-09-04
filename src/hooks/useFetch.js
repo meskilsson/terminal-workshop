1f132a7adad1ea712904fcf18aec86a2e5e8b36a
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2025-09-04 12:03:46 +0200

    vad vill jag lära mig av kursen

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,2 @@
+// Vill lära mig så mycket jag kan få ut av utbildningen, servrar, serverhantering, webbutveckling, react.
+// Framtida mål är att få ett jobb som utmanar mig och lär mig ytterligare
